87d2ef287171914eeced786068ec0bb36dd87f40
Bogdan <bogdan.grigorescu@arnia.ro>
2019-08-06 11:12:46 +0300

    Init commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import "./_variables.scss";
+import "./_reset.scss";
+
+ReactDOM.render(<App />, document.getElementById("root"));
