5a2703f703d2d763a7f3911b6810b40b65f67165
Bogdan <bogdan.grigorescu@arnia.ro>
2019-08-08 19:16:10 +0300

    Emit redux events for OUTCOME_STATUS, MARKET_STATUS & PRICE_CHANGE

--- src/redux/sagas/handleSocketEvents.js
+++ src/redux/sagas/handleSocketEvents.js
@@ -1,5 +1,67 @@
-import { takeEvery, put, all } from "redux-saga/effects";
-import _ from "lodash";
+import { takeEvery, put } from "redux-saga/effects";
+
+function* handleSubscriptionEvents({ payload }) {
+  if (payload.type === "OUTCOME_STATUS") {
+    yield put({ type: "CHANGE_OUTCOME_STATUS", data: payload.data });
+
+    // data: {
+    //  eventId: 21249937,
+    //  marketId: 93649502,
+    //  outcomeId: 367529410,
+    //  price: {
+    //    decimal: '34',
+    //    num: '33',
+    //    den: '1'
+    //  },
+    //  status: {
+    //    active: true,
+    //    resulted: false,
+    //    cashoutable: false,
+    //    displayable: true,
+    //    suspended: true,
+    //    result: '-'
+    //  }
+  }
+
+  if (payload.type === "MARKET_STATUS") {
+    yield put({ type: "CHANGE_MARKET_STATUS", data: payload.data });
+    //  data: {
+    //   eventId: 21249937,
+    //   marketId: 93648886,
+    //   status: {
+    //     active: true,
+    //     resulted: false,
+    //     cashoutable: false,
+    //     displayable: true,
+    //     suspended: true,
+    //     noExtraTime: false,
+    //     live: true
+    //   }
+    // }
+  }
+
+  if (payload.type === "PRICE_CHANGE") {
+    yield put({ type: "PRICE_CHANGE_STATUS", data: payload.data });
+    //     data: {
+    //   eventId: 21249937,
+    //   marketId: 93649141,
+    //   outcomeId: 367528101,
+    //   price: {
+    //     decimal: '1.11',
+    //     num: '1',
+    //     den: '9'
+    //   },
+    //   status: {
+    //     active: true,
+    //     resulted: false,
+    //     cashoutable: false,
+    //     displayable: true,
+    //     suspended: true,
+    //     result: '-'
+    //   }
+    // }
+  }
+}
 
 function* receiveSocketEvents({ payload }) {
   if (payload.type === "LIVE_EVENTS_DATA") {
@@ -25,6 +87,8 @@ function* receiveSocketEvents({ payload }) {
       errorMessage: payload.data.message
     });
   }
+
+  yield* handleSubscriptionEvents({ payload });
 }
 
 function* receiveSocketEventsWatcher() {
